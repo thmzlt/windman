12795de1f911f9da6076336176aa86aa935f1352
Thomaz Leite <1@thomazleite.com>
2020-06-07 22:54:31 -0300

    Copy 2bwm repository
    Also, add some customizations to config.h and disable sloppy focus by
    default.

---
+++ default.nix
@@ -0,0 +1,22 @@
+{ pkgs ? import (fetchTarball https://github.com/nixos/nixpkgs/archive/master.tar.gz) {} }:
+
+pkgs.stdenv.mkDerivation rec {
+  version = "0.3";
+  pname = "2bwm";
+
+  src = ./.;
+
+  #src = pkgs.fetchFromGitHub {
+  #  owner = "venam";
+  #  repo = "2bwm";
+  #  rev = "v${version}";
+  #  sha256 = "1xwib612ahv4rg9yl5injck89dlpyp5475xqgag0ydfd0r4sfld7";
+  #};
+
+  # Allow users set their own list of patches
+  #inherit patches;
+
+  buildInputs = [ pkgs.xorg.libxcb pkgs.xorg.xcbutilkeysyms pkgs.xorg.xcbutilwm pkgs.xorg.libX11 pkgs.xorg.xcbutil pkgs.xcbutilxrm ];
+
+  installPhase = "make install DESTDIR=$out PREFIX=\"\"";
+}
